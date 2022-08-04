a5bbcdacd6acd1037f74b30a496acc2cdc170394
hmuaz <muazsenol1@gmail.com>
2022-08-04 16:54:28 +0300

    Camera_Positions

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -15,13 +15,38 @@ public class PlayerMovement : MonoBehaviour
     {
         karakterinPozisyonu = transform.position;
 
-        if(karakterinPozisyonu.y < -5)
+        
+        if(karakterinPozisyonu.y > -5)
         {
+            float camx = Mathf.Clamp(transform.position.x, -2, 2);
+            float camy = Mathf.Clamp(transform.position.y, 0, 2);
+
+            cam.transform.position = new Vector3(camx, camy, -10);
+        }
+
+        if (karakterinPozisyonu.y < -5)
+        {
+            if(karakterinPozisyonu.y < -18.5f)
+            {
+                float camy = Mathf.Clamp(transform.position.y, -40, -23);
+                float camx = Mathf.Clamp(transform.position.x, -22, 30);
+
+                cam.transform.position = new Vector3(camx, camy, -10);
+            }
+            else
+            {
+                float camy = Mathf.Clamp(transform.position.y, -13.75f, -9.5f);
+                cam.transform.position = new Vector3(0, camy, -10);
+            }
+
+            
 
         }
 
+        
 
-       movement.x = Input.GetAxisRaw("Horizontal");
+
+        movement.x = Input.GetAxisRaw("Horizontal");
        movement.y = Input.GetAxisRaw("Vertical");
 
         animator.SetFloat("Horizontal", movement.x);
